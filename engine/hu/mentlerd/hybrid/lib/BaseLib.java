ea28c412806d71b4b62da6ed8b65f311b3d448f2
David Mentler <mentlerd@gmail.com>
2013-07-04 11:21:24 +0200

    Fix UNPACK not behaving

--- engine/hu/mentlerd/hybrid/lib/BaseLib.java
+++ engine/hu/mentlerd/hybrid/lib/BaseLib.java
@@ -215,13 +215,13 @@ public enum BaseLib implements Callable{
 			
 			int start = frame.getIntArg(1, 1);
 			int limit = frame.getIntArg(2, args.size());
+		
+			if ( start < 1 || limit < 0 )
+				throw new LuaException( "invalid unpack bounds" );
 			
 			if ( limit == 0 )
 				return 0;
-			
-			if ( start < 1 || limit < start )
-				throw new LuaException( "invalid unpack bounds" );
-			
+					
 			int count = limit - start +1;
 			
 			frame.setTop( count );
